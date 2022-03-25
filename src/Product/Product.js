f47efa5c41308c0812c58b6f77695204372e9bc4
herohossen <delowarhossen1224@gmail.com>
2022-03-26 05:11:38 +0600

    3rd commit card added

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './Product.css'
 
-const Product = ({product}) => {
-
+const Product = ({product,handleAddToCart}) => {
+console.log(product);
     // const []=props.product;
     const {name, img,brand,color,price} =product;
+
     return (
         <div>
- <div class="product-container">
+ <div className="product-container">
   <img src={img} alt="Avatar"/>
-  <div class="container">
-    <h4>{name}</h4> 
-    <h4>{brand}</h4> 
-    <h4>{color}</h4> 
-    <h4>{price}</h4> 
-    
-  </div>
+<div className='product-info'>
+<p className="product-name">{name}</p>
+<p className="product-brand">{brand}</p>
+<p className="product-color">{color}</p>
+<p className="product-price">{price}</p>
+</div>
+<button onClick={()=>handleAddToCart(product)}  className='btn-cart'>
+    <p >Add to cart</p>
+</button>
 </div>
 
         </div>
--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -4,22 +4,33 @@ import './Shop.css'
 
 const Shop = () => {
     const [products,setProducts]=useState([]);
+    const [cart,setCart]=useState([]);
 
     useEffect( ()=>{
         fetch('products.json')
         .then(res=> res.json())
         .then(data =>setProducts(data))
     }, []);
+    const handleAddToCart = (product)=>{
+        // console.log(product);
+        const  newCart=[...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
        <div className='products-container'> 
         {
-            products.map(product =>(<Product key={product.id} product={product}></Product>))            
+            products.map(product =>(<Product key={product.id} 
+                product={product}
+                handleAddToCart={handleAddToCart}
+                >
+                    
+                </Product>))            
         }
 
        </div>
-       <div className='order-container'> 
-       <h1>My Order</h1>
+       <div className='cart-container'> 
+       Cart: item {cart.length}
 </div>
         </div>
     );
